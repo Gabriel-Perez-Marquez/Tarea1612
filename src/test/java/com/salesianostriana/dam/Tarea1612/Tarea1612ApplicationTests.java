319a9394fb984371bdb34c0ae979e544ff1c51e4
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2025-12-16 11:45:13 +0100

    Proyecto iniciado

---
+++ src/test/java/com/salesianostriana/dam/Tarea1612/Tarea1612ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.Tarea1612;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea1612ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
